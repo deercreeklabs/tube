b696fd7b93b3f29f67f05e44f2880f7f020b18dd
Chad Harrington <chad@deercreeklabs.com>
2017-08-24 16:13:51 -0600

    Fix dep conflicts.

--- project.clj
+++ project.clj
@@ -9,13 +9,15 @@
   {:dev
    {:plugins
     [[lein-ancient "0.6.10"]
-     [lein-cljsbuild "1.1.7"]
-     [lein-cloverage "1.0.9"]
+     [lein-cljsbuild "1.1.7" :exclusions [org.clojure/clojure]]
+     [lein-cloverage "1.0.9" :exclusions [org.clojure/clojure]]
      ;; Because of confusion with a defunct project also called
      ;; lein-release, we exclude lein-release from lein-ancient.
      [lein-release "1.0.9" :upgrade false :exclusions [org.clojure/clojure]]]}
    :uberjar {:aot :all
              :jvm-opts ^:replace ["-server" "-XX:+AggressiveOpts"]}}
+
+  :pedantic? :abort
 
   :dependencies
   [[bidi "2.1.2"]
@@ -23,7 +25,7 @@
    [cljsjs/pako "0.2.7-0"]
    [clj-time "0.14.0"]
    [com.andrewmcveigh/cljs-time "0.5.1"]
-   [com.google.guava/guava "23.0"]
+   [com.google.guava/guava "23.0" :exclusions [com.google.code.findbugs/jsr305]]
    [com.taoensso/timbre "4.10.0"]
    [http-kit "2.2.0"]
    [org.clojure/clojure "1.8.0"]
