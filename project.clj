1e8480654684821d2608b7a965d81ff53b3c7b15
Chad Harrington <chad@deercreeklabs.com>
2017-09-07 00:49:49 -0600

    lein-release plugin: preparing 0.1.3 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject deercreeklabs/tube "0.1.3-SNAPSHOT"
+(defproject deercreeklabs/tube "0.1.3"
   :description "Clojure/Clojurescript websocket client and server library."
   :url "http://www.deercreeklabs.com"
   :license {:name "Apache License, Version 2.0"
