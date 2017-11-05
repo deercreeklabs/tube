4bb2f9f5acf5f8aeafa56ccc64047a371d1aba95
Chad Harrington <chad@deercreeklabs.com>
2017-11-04 21:27:44 -0600

    lein-release plugin: bumped version from 0.1.9 to 0.1.10-SNAPSHOT for next development cycle

--- project.clj
+++ project.clj
@@ -40,7 +40,7 @@
              :compiler compiler}
       node-test? (assoc :notify-command ["node" output-to]))))
 
-(defproject deercreeklabs/tube "0.1.9"
+(defproject deercreeklabs/tube "0.1.10-SNAPSHOT"
   :description "Clojure/Clojurescript websocket client and server library."
   :url "http://www.deercreeklabs.com"
   :license {:name "Apache License, Version 2.0"
