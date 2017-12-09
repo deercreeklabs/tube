8bfeb8678620be82bf590c0bceaf8474002aaa33
Chad Harrington <chad@deercreeklabs.com>
2017-12-09 13:59:52 -0700

    lein-release plugin: preparing 0.1.14 release

--- project.clj
+++ project.clj
@@ -39,7 +39,7 @@
              :compiler compiler}
       node-test? (assoc :notify-command ["node" output-to]))))
 
-(defproject deercreeklabs/tube "0.1.14-SNAPSHOT"
+(defproject deercreeklabs/tube "0.1.14"
   :description "Clojure/Clojurescript websocket client and server library."
   :url "http://www.deercreeklabs.com"
   :license {:name "Apache License, Version 2.0"
@@ -48,7 +48,8 @@
                  :deploy-via :clojars}
 
   :main deercreeklabs.tube.server
-  ;;:pedantic? :abort
+
+  :pedantic? :abort
 
   :profiles
   {:dev
@@ -88,7 +89,6 @@
    [deercreeklabs/async-utils "0.1.6"]
    [deercreeklabs/baracus "0.1.1"]
    [deercreeklabs/log-utils "0.1.2"]
-   [less-awful-ssl "1.0.1"]
    [org.clojure/clojure "1.9.0"]
    [org.clojure/clojurescript "1.9.946"]
    [org.clojure/core.async "0.3.465"]
