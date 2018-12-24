1446cbda996b76c4a1483859d3dfd5e83fdef6c0
Chad Harrington <chad@deercreeklabs.com>
2018-12-24 12:54:00 -0700

    Upgrade deps.

--- project.clj
+++ project.clj
@@ -88,7 +88,7 @@
    [com.taoensso/timbre "4.10.0"]
    [deercreeklabs/async-utils "0.1.14"]
    [deercreeklabs/baracus "0.1.14"]
-   [deercreeklabs/log-utils "0.2.3"]
+   [deercreeklabs/log-utils "0.2.4"]
    [http-kit "2.3.0"]
    [org.clojure/clojure "1.10.0"]
    [org.clojure/clojurescript "1.10.439"]
